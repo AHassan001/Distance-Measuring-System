96a542697076017f06202ffabb23ad7ed81bac3d
Abdelrahman Maher <a.maher.hasan@gmail.com>
2024-01-03 17:30:28 +0200

    Adding Distance Measuring System Project

---
+++ Mini_project4_eclipse/Mini_Project_4/Debug/Mini_Project4.d
@@ -0,0 +1,8 @@
+Mini_Project4.o Mini_Project4.o: ../Mini_Project4.c ../lcd.h \
+  ../std_types.h ../ultra_sonic.h
+
+../lcd.h:
+
+../std_types.h:
+
+../ultra_sonic.h:
---
+++ Mini_project4_eclipse/Mini_Project_4/Debug/ultra_sonic.d
@@ -0,0 +1,10 @@
+ultra_sonic.o ultra_sonic.o: ../ultra_sonic.c ../ultra_sonic.h \
+  ../std_types.h ../icu.h ../gpio.h
+
+../ultra_sonic.h:
+
+../std_types.h:
+
+../icu.h:
+
+../gpio.h:
